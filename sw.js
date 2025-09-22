13d0e56ffecfa3faa9dac8339389a6f8c90d9ba4
mysengames <mysengames@gmail.com>
2025-09-22 20:57:13 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open('ezdb-v5').then(function(cache) {
-    return cache.addAll([
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('ezdb-v8').then(cache => cache.addAll([
       './',
       './index.html',
       './manifest.json',
       './icons/icon-192.png',
       './icons/icon-512.png',
-      './data/items.json',
-      './data/monsters.json'
-    ]);
-  }));
+      './data/items_daggers.json'  // ← 短剣のみ
+    ]))
+  );
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(resp) {
-    return resp || fetch(e.request);
-  }));
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
