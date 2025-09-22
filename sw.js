7d16cd7f83b30ad044333ac807cfd4b1032443f9
mysengames <mysengames@gmail.com>
2025-09-22 21:01:06 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('ezdb-v8').then(cache => cache.addAll([
+    caches.open('ezdb-v9').then(cache => cache.addAll([
       './',
       './index.html',
       './manifest.json',
       './icons/icon-192.png',
       './icons/icon-512.png',
-      './data/items_daggers.json'  // ← 短剣のみ
+      './data/items_daggers.json',
+      // 後で長剣や防具を追加したらここに追記
+      './data/monsters.json'
     ]))
   );
 });
