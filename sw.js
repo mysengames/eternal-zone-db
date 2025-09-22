81f066f8fb661d0da64534f0cc2555e96dbb2b0b
mysengames <mysengames@gmail.com>
2025-09-22 19:00:04 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('ezdb-v3').then(cache => cache.addAll([
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png',
-    './data/items.json',
-    './data/monsters.json'
-  ])));
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open('ezdb-v4').then(function(cache) {
+    return cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icons/icon-192.png',
+      './icons/icon-512.png',
+      './data/items.json',
+      './data/monsters.json'
+    ]);
+  }));
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(resp) {
+    return resp || fetch(e.request);
+  }));
 });
